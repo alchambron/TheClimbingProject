f658c9cc9676d2cd737e7cc7c3002c4060a54f8c
alchambron <alexis.chambron@gmail.com>
2022-12-07 18:21:37 +0100

    Fixing Asset Pipeline Javscripts Bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,10 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile = ["manifest.js"]
+
+
+Rails.application.config.assets.paths << Rails.root.join("lib", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("lib", "assets", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
