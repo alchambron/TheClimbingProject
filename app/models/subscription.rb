fee437718679d5a3d40f7b4ab37046dabf7759af
Thomas Raducha <thomasraducha@gmail.com>
2022-12-07 16:45:25 +0100

    update scaffold

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,3 @@
 class Subscription < ApplicationRecord
+  has_many :order_subcriptions
 end
--- db/migrate/20221207153416_create_subscriptions.rb
+++ db/migrate/20221207153416_create_subscriptions.rb
@@ -1,8 +1,8 @@
 class CreateSubscriptions < ActiveRecord::Migration[7.0]
   def change
-    create_table :subscriptions do |t|
-      t.string :name
-      t.integer :price
+    create_table(:subscriptions) do |t|
+      t.string(:name)
+      t.integer(:price)
 
       t.timestamps
     end
