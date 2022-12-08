dc11d87c3631f7adf94be7ec4315bddcb642e1a2
alchambron <alexis.chambron@gmail.com>
2022-12-08 17:50:21 +0100

    Adding And Configurate Administrate

---
+++ app/dashboards/user_dashboard.rb
@@ -0,0 +1,102 @@
+require "administrate/base_dashboard"
+
+class UserDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    birth_date: Field::DateTime,
+    center_id: Field::Number,
+    courses: Field::HasMany,
+    email: Field::String,
+    encrypted_password: Field::String,
+    first_name: Field::String,
+    is_admin: Field::Boolean,
+    last_name: Field::String,
+    order_courses: Field::HasMany,
+    order_subscriptions: Field::HasMany,
+    remember_created_at: Field::DateTime,
+    reset_password_sent_at: Field::DateTime,
+    reset_password_token: Field::String,
+    subscriptions: Field::HasMany,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    birth_date
+    center_id
+    courses
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    birth_date
+    center_id
+    courses
+    email
+    encrypted_password
+    first_name
+    is_admin
+    last_name
+    order_courses
+    order_subscriptions
+    remember_created_at
+    reset_password_sent_at
+    reset_password_token
+    subscriptions
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    birth_date
+    center_id
+    courses
+    email
+    encrypted_password
+    first_name
+    is_admin
+    last_name
+    order_courses
+    order_subscriptions
+    remember_created_at
+    reset_password_sent_at
+    reset_password_token
+    subscriptions
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how users are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(user)
+  #   "User ##{user.id}"
+  # end
+end
