230300e7e0a386ebfb6a50275f561516d6dff4f2
thimibka <fseetp@gmail.com>
2022-12-08 15:58:46 +0100

    adding feature_view_restauration

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :courses
   resources :order_courses
   resources :order_subscriptions
+  resources :restaurations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
