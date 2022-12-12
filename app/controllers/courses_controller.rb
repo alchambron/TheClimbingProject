5663614a95707e4043a40b71efc4c1556321fe01
Thomas Raducha <thomasraducha@gmail.com>
2022-12-12 12:38:45 +0100

    no redirect course if past

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,10 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
+    # It's a variable that is used to determine the start date of the week.
     monday_date = params.fetch(:start_date, Date.today).to_date.beginning_of_week(:monday)
+    date_today = Date.today
+    redirect_back(fallback_location: root_path) if monday_date < date_today
     courses = Course.all
     @course_events = courses.map { |c| CourseEvent.from_course(c, monday_date) }
   end
