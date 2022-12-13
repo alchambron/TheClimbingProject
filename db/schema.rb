086510fd193c6b689461dd6e70489e4351f158bf
alchambron <alexis.chambron@gmail.com>
2022-12-13 23:05:30 +0100

    Fixing Table Issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_154711) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_195612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,7 +105,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_154711) do
   create_table "order_subscriptions", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "subscription_id", null: false
-    t.string "subscription_end_date"
     t.string "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
