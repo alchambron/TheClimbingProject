8ce0985dcee7d38877fb4f1ba36366c5b51b2920
alchambron <alexis.chambron@gmail.com>
2022-12-13 16:54:25 +0100

    Adding Column Duration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_095545) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_154711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,6 +147,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_095545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "duration"
   end
 
   create_table "users", force: :cascade do |t|
