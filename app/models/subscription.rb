d9e57a11f378b762d4c1e40888329baa72af1153
Belen <gonzaleznovoa.b@protonmail.com>
2022-12-15 23:21:29 +0100

    Adding validations to course and subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,7 @@
 class Subscription < ApplicationRecord
   has_many :order_subscriptions
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :duration, presence: true
 end
