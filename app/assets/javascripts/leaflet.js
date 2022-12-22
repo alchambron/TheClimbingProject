94db53b705d7d70e1fa3e8d1308f4f6556c19478
Thomas Raducha <thomasraducha@gmail.com>
2022-12-22 20:02:26 +0100

    reverse latitude longitude

--- app/assets/javascripts/leaflet.js
+++ app/assets/javascripts/leaflet.js
@@ -2,11 +2,13 @@ const mappy = document.getElementById("map")
 
 let longitude = parseFloat(mappy.getAttribute("data-longitude"))
 let latitude = parseFloat(mappy.getAttribute("data-latitude"))
-console.log(longitude, latitude)
-let map = L.map('map').setView([longitude, latitude], 13);
+
+const coords = [latitude, longitude]
+console.log(coords)
+let map = L.map('map').setView(coords, 12);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([longitude, latitude]).addTo(map);
+let marker = L.marker(coords).addTo(map);
